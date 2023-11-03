01582c3e1f3b96dbfd47187173771597e0666a71
alfianElsa14 <alfian.elsa@phincon.com>
2023-11-03 14:15:50 +0700

    styling detail page

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import style from '../style/details.module.scss'
+import axios from 'axios'
 
 
 function Detail() {
+    const [detailBook, setDetailBook] = useState()
+
+    async function fetchBookById() {
+        try {
+            const response = await axios.get(`http://localhost:3000/books/1`)
+            setDetailBook(response.data)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchBookById()
+    }, [])
+
     return (
         <div className={style.containerDetail}>
-            <h1>Detail Pages</h1>
+            <h1>Detail Page</h1>
             <div className={style.content}>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaJkvh-di8jG7jLfRy87sqSboqb5tf1h5NpQ&usqp=CAU" alt="" />
+                <img src={detailBook?.image} alt="" />
                 <div className={style.desricption}>
-                    <p>Title: </p>
-                    <p>Description: </p>
-                    <p>Author: </p>
-                    <p>Favorites: </p>
+                    <p>Title: {detailBook?.title}</p>
+                    <p>Description: {detailBook?.description}</p>
+                    <p>Category: {detailBook?.category}</p>
+                    <p>Favorites: {detailBook?.favorite}</p>
+                    <button>Edit</button>
+                    <button>delete</button>
                 </div>
-
             </div>
         </div>
     )
